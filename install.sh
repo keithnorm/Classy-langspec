bfc6652c22f458f132ca9a98a972417764a2b098
Keith Norman <keithnorm@gmail.com>
2015-06-08 14:25:04 -0700

    download plist file before merging

--- install.sh
+++ install.sh
@@ -10,10 +10,14 @@ PLISTBUDDY=/usr/libexec/PlistBuddy
 
 # This framework is found withing the Xcode.app package and is used when Xcode is a monolithic
 # install (all contained in Xcode.app)
+
+
+
 DVTFOUNDATION_PATH="/Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/"
+curl "https://raw.githubusercontent.com/keithnorm/Classy-langspec/master/CAS.plist" -o "temp.plist"
 cp "$DVTFOUNDATION_PATH/DVTFoundation.xcplugindata" "$DVTFOUNDATION_PATH/DVTFoundation.xcplugindata.bak"
 # Now merge in the additonal languages to DVTFoundation.xcplugindata
-$PLISTBUDDY "$DVTFOUNDATION_PATH/DVTFoundation.xcplugindata"  -c 'Merge CAS.plist plug-in:extensions'
+$PLISTBUDDY "$DVTFOUNDATION_PATH/DVTFoundation.xcplugindata"  -c 'Merge temp.plist plug-in:extensions'
 
 LANGSPEC=$(cat $SCRIPT_PATH/CAS.xclangspec)
 
@@ -22,5 +26,6 @@ echo "$LANGSPEC" > "$DVTFOUNDATION_PATH/CAS.xclangspec"
 
 # Remove any cached Xcode plugins
 rm -rf /private/var/folders/*/*/*/com.apple.DeveloperTools/*/Xcode/PlugInCache*.xcplugincache
+rm temp.plist
 
 echo "Done"
